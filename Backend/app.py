e9eaf990dc01a588ee27b86364bac7f61022ec4b
Thuwapat <96274678+Thuwapat@users.noreply.github.com>
2025-03-12 14:05:38 +0700

    Fix Google Login

--- Backend/app.py
+++ Backend/app.py
@@ -10,7 +10,7 @@ from requests_oauthlib import OAuth2Session
 from oauthlib.oauth2 import LegacyApplicationClient
 
 app = Flask(__name__)
-CORS(app, origins=["http://localhost:3000"])
+CORS(app, origins=["http://localhost:3000", "https://localhost:3000"])
 
 app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'postgresql://myuser:mypass@localhost:5432/mydb')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
